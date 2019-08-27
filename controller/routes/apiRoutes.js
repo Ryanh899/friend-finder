9f93a4e0edcb768b694c6719d4b67c32ed48cad9
Ryan Hazzard <rhazzardphoto@gmail.com>
2019-08-27 00:21:10 -0700

    everything works, need to add duplicate function, need to make modal that shows best match

--- controller/routes/apiRoutes.js
+++ controller/routes/apiRoutes.js
@@ -13,24 +13,73 @@ module.exports = (express) => {
             return arr.join('')
         }
     };
+    var getId = (num, arr) => {
+        return arr.indexOf(num)
+    }
+    var findDifferences = (arr) => {
+        finalDifference = [];
+        otherDif = [];
+        valuesArr = [];
+        idArr = [];
+        let chop = arr.splice(arr.length - 9, arr.length);
+        while (arr.length) {
+            finalDifference.push(arr.splice(0, 9));
+        };
+        // console.log(finalDifference);
+        for (var i = 0; i < finalDifference.length; i++) {
+            let num = 0;
+            for (var n = 0; n < finalDifference[i].length; n++) {
+                num += Math.abs(finalDifference[i][n]);
+            };
+            otherDif.push({
+                pointsOff: num,
+                id: finalDifference.indexOf(finalDifference[i])
+            });
+        };
+        test(otherDif, valuesArr, idArr);
+        low = Array.min(valuesArr);
+        testId = valuesArr.indexOf(low) + 1;
+        return {
+            bestMatch: low,
+            id: testId
+        };
+    };
+    Array.min = function (array) {
+        return Math.min.apply(Math, array);
+    };
+    var test = (useArr, pushArr, id) => {
+        for (var i = 0; i < useArr.length; i++) {
+            temp = Object.values(useArr[i]);
+            let pop = temp.pop();
+            id.push({
+                id: pop
+            })
+            pushArr.push(temp[0]);
+        };
+    };
+
     var findMatch = () => {
+        responses = [];
+        differences = [];
         knex.select().table('users')
             .then((result) => {
-                console.log(result.length); 
-                for (var i = 0; i < result.length; i++) {
-                    var string = JSON.stringify(result[i].responses);
-                    let split = string.split('');
-                    var secondString = JSON.stringify(result[i++].responses);
-                    let secondSplit = secondString.split('');
-                    for (var i = 0; i < split.length; i++) {
-                        for (var i = 0; i < secondSplit.length; i++) {
-                            let num1 = split[i];
-                            let num2 = secondSplit[i];
-                            let difference = num1- num2
-                            console.log(difference)
-                        };
+                var resSpl
+                result.forEach(element => {
+                    var resStr = JSON.stringify(element.responses);
+                    resSpl = resStr.split('');
+                    responses.push(resSpl);
+                });
+                for (var i = 0; i < responses.length; i++) {
+                    let newArr = responses[responses.length - 1];
+                    for (var n = 0; n < responses[i].length; n++) {
+                        let num = responses[responses.length - 1][n];
+                        let num2 = responses[i][n]
+                        // console.log(`${num} - ${num2} = ${num - num2}`);
+                        differences.push(num - num2);
                     };
                 };
+                // findDifferences(differences); 
+                console.log(findDifferences(differences))
             })
             .catch((err) => {
                 if (err) throw err
